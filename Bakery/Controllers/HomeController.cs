9653ace2346676bcb61ebc74db959ec4b5de3be6
Kim Brannian <brannian@gmail.com>
2022-01-15 10:50:07 -0800

    update database

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bakery.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BakeryContext _db;
+
+        public HomeController(BakeryContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+        ViewData["flavors"]= _db.Flavors.ToList();
+        ViewData["treats"] = _db.Treats.ToList();
+        return View();
+       }
+    }
+}
